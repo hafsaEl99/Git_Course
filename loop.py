2e9109d0db9f349d0e2664dcdc06a6422de512bd
Hafsa Eldakrory <hmldkrry@memphis.edu>
2025-02-14 05:01:07 -0600

    set range loop

--- loop.py
+++ loop.py
@@ -1,2 +1,3 @@
-This file was created in the origin for the purpose of pulling it. DON'T FORGET TO COMMIT IT USING THE COMMIT BUTTON—NO TERMINAL USE HERE!
-
+#This file was created in the origin for the purpose of pulling it. DON'T FORGET TO COMMIT IT USING THE COMMIT BUTTON—NO TERMINAL USE HERE!
+for i in range(10):
+    print(i)
